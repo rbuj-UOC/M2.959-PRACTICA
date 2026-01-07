8e08280884ec60dbd3da282d3f15f7c595985256
Robert Buj <robert.buj@gmail.com>
2026-01-07 22:04:07 +0100

    add demography evolution chart

---
+++ src/scripts/charts/demographic-chart.js
@@ -0,0 +1,218 @@
+import * as d3 from 'd3';
+
+export function createDemographicChart(data) {
+    const container = d3.select('#chart-demographic');
+    const provinciaSelect = d3.select('#demographic-provincia');
+    const comarquesContainer = d3.select('#demographic-comarques');
+
+    // Format numèric amb separador de milers
+    const formatNumber = (value, decimals = 0) => {
+        const fixed = Number(value).toFixed(decimals);
+        const [intPart, decPart] = fixed.split('.');
+        const withThousands = intPart.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+        return decimals > 0 ? `${withThousands},${decPart}` : withThousands;
+    };
+
+    // Agrupar dades per província i comarca per any
+    const dataByProvinciaComarca = {};
+
+    data.forEach(d => {
+        if (!dataByProvinciaComarca[d.provincia]) {
+            dataByProvinciaComarca[d.provincia] = {};
+        }
+        if (!dataByProvinciaComarca[d.provincia][d.comarca]) {
+            dataByProvinciaComarca[d.provincia][d.comarca] = {};
+        }
+        const year = d.any;
+        if (!dataByProvinciaComarca[d.provincia][d.comarca][year]) {
+            dataByProvinciaComarca[d.provincia][d.comarca][year] = d.poblacio_total;
+        }
+    });
+
+    console.log('Mostra de dades:', data.slice(0, 3));
+    console.log('Dades per província i comarca:', dataByProvinciaComarca);
+
+    // Obtenir les comarques úniques per província
+    const getComarques = (provincia) => {
+        return Object.keys(dataByProvinciaComarca[provincia] || {}).sort();
+    };
+
+    // Actualitzar les comarques disponibles
+    function updateComarques(provincia) {
+        comarquesContainer.html('');
+
+        // Si la província és "Totes", no es mostren les caselles
+        if (provincia === 'Totes') {
+            return;
+        }
+
+        const comarques = getComarques(provincia);
+
+        comarques.forEach(comarca => {
+            const label = comarquesContainer.append('label')
+                .style('display', 'block')
+                .style('margin', '0.5rem 0')
+                .style('cursor', 'pointer');
+
+            const checkbox = label.append('input')
+                .attr('type', 'checkbox')
+                .attr('value', comarca)
+                .attr('id', `comarca-${comarca}`)
+                .attr('data-comarca', comarca)
+                .style('margin-right', '0.5rem');
+
+            label.append('span')
+                .text(comarca);
+        });
+
+        // Afegir l'event listener a totes les caselles de selecció
+        comarquesContainer.selectAll('input[type="checkbox"]')
+            .on('change', render);
+    }
+
+    function render() {
+        const provincia = provinciaSelect.property('value');
+
+        let chartData = [];
+
+        // Obtenir els anys disponibles (2006-2021)
+        const years = Array.from({ length: 16 }, (_, i) => 2006 + i);
+
+        // Si es "Totes", mostrem la suma de la població de tota Catalunya
+        if (provincia === 'Totes') {
+            const values = years.map(year => {
+                let totalPopulation = 0;
+
+                // Sumar la població de totes les comarques de totes les províncies
+                Object.keys(dataByProvinciaComarca).forEach(prov => {
+                    Object.keys(dataByProvinciaComarca[prov]).forEach(comarca => {
+                        totalPopulation += dataByProvinciaComarca[prov][comarca][year] || 0;
+                    });
+                });
+
+                return { year, population: totalPopulation };
+            });
+
+            chartData = [{ comarca: 'Catalunya', values }];
+        } else {
+            // Obtenir les comarques sel·leccionades
+            let selectedComarques = comarquesContainer.selectAll('input[type="checkbox"]:checked')
+                .nodes()
+                .map(node => node.value);
+
+            // Si no hi ha cap comarca sel·leccionada, sel·leccionar totes de la província
+            if (selectedComarques.length === 0) {
+                selectedComarques = getComarques(provincia);
+            }
+
+            // Preparar dades per al gràfic
+            chartData = selectedComarques.map(comarca => {
+                const values = years.map(year => ({
+                    year,
+                    population: dataByProvinciaComarca[provincia][comarca][year] || 0
+                }));
+                return { comarca, values };
+            });
+        }
+
+        // Dimensions
+        const margin = { top: 20, right: 30, bottom: 30, left: 60 };
+        const width = container.node().clientWidth - margin.left - margin.right;
+        const height = 400 - margin.top - margin.bottom;
+
+        // Neteja
+        container.html('');
+
+        // SVG
+        const svg = container
+            .append('svg')
+            .attr('width', width + margin.left + margin.right)
+            .attr('height', height + margin.top + margin.bottom)
+            .append('g')
+            .attr('transform', `translate(${margin.left},${margin.top})`);
+
+        // Escales
+        const xScale = d3.scaleLinear()
+            .domain(d3.extent(years))
+            .range([0, width]);
+
+        const yMax = d3.max(chartData, d => d3.max(d.values, v => v.population));
+        const yScale = d3.scaleLinear()
+            .domain([0, yMax])
+            .range([height, 0]);
+
+        const colors = d3.scaleOrdinal(d3.schemeCategory10);
+
+        // Generador de línies
+        const line = d3.line()
+            .x(d => xScale(d.year))
+            .y(d => yScale(d.population));
+
+        // Eix X
+        svg.append('g')
+            .attr('transform', `translate(0,${height})`)
+            .call(d3.axisBottom(xScale).tickFormat(d3.format('d')))
+            .append('text')
+            .attr('x', width / 2)
+            .attr('y', 30)
+            .attr('fill', '#333')
+            .attr('text-anchor', 'middle')
+            .text('Any');
+
+        // Eix Y
+        svg.append('g')
+            .call(d3.axisLeft(yScale).tickFormat(d => formatNumber(d)))
+            .append('text')
+            .attr('transform', 'rotate(-90)')
+            .attr('x', -height / 2)
+            .attr('y', -45)
+            .attr('fill', '#333')
+            .attr('text-anchor', 'middle')
+            .text('Població');
+
+        // Línies
+        chartData.forEach((d, i) => {
+            svg.append('path')
+                .datum(d.values)
+                .attr('fill', 'none')
+                .attr('stroke', colors(i))
+                .attr('stroke-width', 2)
+                .attr('d', line);
+        });
+
+        // Llegenda a sota de l'eix X, alineada verticalment
+        const legend = svg.selectAll('.legend')
+            .data(chartData)
+            .enter()
+            .append('g')
+            .attr('class', 'legend')
+            .attr('transform', (d, i) => `translate(0, ${height + 50 + i * 20})`);
+
+        legend.append('rect')
+            .attr('width', 10)
+            .attr('height', 10)
+            .attr('fill', (d, i) => colors(i));
+
+        legend.append('text')
+            .attr('x', 15)
+            .attr('y', 8)
+            .attr('font-size', '12px')
+            .text(d => d.comarca);
+    }
+
+    // Inicialitzar amb "Totes"
+    const firstProvincia = 'Totes';
+    provinciaSelect.property('value', firstProvincia);
+    updateComarques(firstProvincia);
+
+    // Event listener per al canvi de província
+    provinciaSelect.on('change', function () {
+        const provincia = d3.select(this).property('value');
+        updateComarques(provincia);
+        render();
+    });
+
+    // Renderització inicial
+    render();
+}
+
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,7 @@ import { initSlideNavigation } from './slides.js';
 // gràfiques
 import * as d3 from 'd3';
 import { createOverviewPieChart } from './charts/overview-pie.js';
+import { createDemographicChart } from './charts/demographic-chart.js';
 
 // Prefix base per a desplegament a GitHub Pages
 const BASE_URL = window.location.origin.includes('rbuj-uoc.github.io')
@@ -22,6 +23,17 @@ Promise.all([
         any: +d['Any'],
         comarca: d['Comarca'],
         kg_per_hab_dia: +d['Total kg/hab./dia'],
+        // Població.x 0-15,Població.y 0-15,Població.x 16-24,Població.y 16-24,Població.x 25-44,Població.y 25-44,Població.x 45-64,Població.y 45-64,Població.x 64+,Població.y 64+,Total kg/hab./dia,Recollida no selectiva,Recollida selectiva
+        poblacio_masculina_0_15: +d['Població.y 0-15'],
+        poblacio_masculina_16_24: +d['Població.y 16-24'],
+        poblacio_masculina_25_44: +d['Població.y 25-44'],
+        poblacio_masculina_45_64: +d['Població.y 45-64'],
+        poblacio_masculina_64_120: +d['Població.y 64+'],
+        poblacio_femenina_0_15: +d['Població.x 0-15'],
+        poblacio_femenina_16_24: +d['Població.x 16-24'],
+        poblacio_femenina_25_44: +d['Població.x 25-44'],
+        poblacio_femenina_45_64: +d['Població.x 45-64'],
+        poblacio_femenina_64_120: +d['Població.x 64+'],
         provincia: d['Província'],
         tones_recollida_no_selectiva: +d['Recollida no selectiva'],
         tones_recollida_selectiva: +d['Recollida selectiva'],
@@ -29,7 +41,11 @@ Promise.all([
         .map(d => ({
             ...d,
             // calcula les tones totals de recollida
-            tones_recollida_total: d.tones_recollida_selectiva + d.tones_recollida_no_selectiva
+            tones_recollida_total: d.tones_recollida_selectiva + d.tones_recollida_no_selectiva,
+            // calcula la població total
+            poblacio_masculina: d.poblacio_masculina_0_15 + d.poblacio_masculina_16_24 + d.poblacio_masculina_25_44 + d.poblacio_masculina_45_64 + d.poblacio_masculina_64_120,
+            poblacio_femenina: d.poblacio_femenina_0_15 + d.poblacio_femenina_16_24 + d.poblacio_femenina_25_44 + d.poblacio_femenina_45_64 + d.poblacio_femenina_64_120,
+            poblacio_total: d.poblacio_masculina_0_15 + d.poblacio_masculina_16_24 + d.poblacio_masculina_25_44 + d.poblacio_masculina_45_64 + d.poblacio_masculina_64_120 + d.poblacio_femenina_0_15 + d.poblacio_femenina_16_24 + d.poblacio_femenina_25_44 + d.poblacio_femenina_45_64 + d.poblacio_femenina_64_120
         }));
 
     // Descartar valors no disponibles en càlculs específics
@@ -41,7 +57,10 @@ Promise.all([
     // Crear gràfica de presentació
     createOverviewPieChart(data);
 
-    console.log('Dades carregades i gràfica inicialitzada');
+    // Crear gràfica de demografia
+    createDemographicChart(data);
+
+    console.log('Dades carregades i gràfiques inicialitzades');
 
 }).catch(error => {
     console.error('Error carregant les dades:', error);
